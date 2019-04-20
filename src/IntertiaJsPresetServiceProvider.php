fb9be97dd53f3fdcec319d2ba095648d71a4846a
Michael Dyrynda <michael@dyrynda.com.au>
2019-04-20 12:58:29 +0930

    Spell 'Inertia' properly 😏

--- src/IntertiaJsPresetServiceProvider.php
+++ src/IntertiaJsPresetServiceProvider.php
@@ -5,12 +5,12 @@ namespace LaravelFrontendPresets\InertiaJsPreset;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Foundation\Console\PresetCommand;
 
-class IntertiaJsPresetServiceProvider extends ServiceProvider
+class InertiaJsPresetServiceProvider extends ServiceProvider
 {
     public function boot()
     {
         PresetCommand::macro('inertiajs', function ($command) {
-            IntertiaJsPreset::install();
+            InertiaJsPreset::install();
 
             $command->info('Inertia.js scaffolding installed successfully.');
             $command->info('Please run "npm install && npm run dev" to compile your fresh scaffolding.');
