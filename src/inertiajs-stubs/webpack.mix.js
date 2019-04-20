45bba8235a1de3f13c2f7c315a506954f4c09e9e
Michael Dyrynda <michael@dyrynda.com.au>
2019-04-20 12:59:39 +0930

    Compile Laravel's default SASS

--- src/inertiajs-stubs/webpack.mix.js
+++ src/inertiajs-stubs/webpack.mix.js
@@ -1,12 +1,13 @@
 const mix = require('laravel-mix')
 const path = require('path')
 
-mix.js('resources/js/app.js', 'public/js').webpackConfig({
-  output: { chunkFilename: 'js/[name].[contenthash].js' },
-  resolve: {
-    alias: {
-      'vue$': 'vue/dist/vue.runtime.js',
-      '@': path.resolve('resources/js'),
-    },
-  },
-})
+mix.sass('resources/sass/app.scss', 'public/css')
+   .js('resources/js/app.js', 'public/js').webpackConfig({
+     output: { chunkFilename: 'js/[name].[contenthash].js' },
+     resolve: {
+       alias: {
+         'vue$': 'vue/dist/vue.runtime.js',
+         '@': path.resolve('resources/js'),
+       },
+     },
+   })
