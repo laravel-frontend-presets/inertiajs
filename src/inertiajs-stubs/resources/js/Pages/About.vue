f6490bcc0d24d25b1be01527257164de36f33614
Michael Dyrynda <michael@dyrynda.com.au>
2019-04-20 13:00:42 +0930

    Add default about, contact page components

---
+++ src/inertiajs-stubs/resources/js/Pages/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <layout>
+    <h1>About</h1>
+    <p>About my first Inertia.js app!</p>
+  </layout>
+</template>
+
+<script>
+import Layout from '@/Shared/Layout'
+
+export default {
+  components: {
+    Layout,
+  },
+}
+</script>
---
+++ src/inertiajs-stubs/resources/js/Pages/Contact.vue
@@ -0,0 +1,16 @@
+<template>
+  <layout>
+    <h1>Contact</h1>
+    <p>Contact me about my first Inertia.js app!</p>
+  </layout>
+</template>
+
+<script>
+import Layout from '@/Shared/Layout'
+
+export default {
+  components: {
+    Layout,
+  },
+}
+</script>
