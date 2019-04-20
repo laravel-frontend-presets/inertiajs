d3cd78bdff4e74c52f7400546f5993106f80d7ac
Michael Dyrynda <michael@dyrynda.com.au>
2019-04-20 13:01:00 +0930

    Add default web routes file

---
+++ src/inertiajs-stubs/routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+});
+
+Route::get('/about', function () {
+    return Inertia::render('About');
+});
+
+Route::get('/contact', function () {
+    return Inertia::render('Contact');
+});
