e79cb57c0f0440b31d4f32160592e2472362b7c2
Michael Dyrynda <michael@dyrynda.com.au>
2019-08-14 09:19:07 +0930

    Swap js/sass lines in webpack config

--- src/inertiajs-stubs/webpack.mix.js
+++ src/inertiajs-stubs/webpack.mix.js
@@ -1,8 +1,8 @@
 const mix = require('laravel-mix')
 const path = require('path')
 
-mix.sass('resources/sass/app.scss', 'public/css')
-   .js('resources/js/app.js', 'public/js').webpackConfig({
+mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css')
      output: { chunkFilename: 'js/[name].[contenthash].js' },
      resolve: {
        alias: {
